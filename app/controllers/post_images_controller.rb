fbf2cff512fccbc44238103fee1214d56932df42
AyakaTakahashi <132245813+suco31a42@users.noreply.github.com>
2023-05-09 08:34:59 +0000

    [finish]4-13

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -18,6 +18,11 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.find(params[:id])
   end
 
+  def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
 
-  resources :post_images, only: [:new, :index, :show, :create]
+  resources :post_images, only: [:new, :index, :show, :create, :destroy]
 
   devise_for :users
 
