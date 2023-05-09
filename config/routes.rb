d2bde1bf337c47bb3999f371dd04b2762569a8f0
AyakaTakahashi <132245813+suco31a42@users.noreply.github.com>
2023-05-09 10:45:50 +0000

    [finish]4-15

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'homes#top'
 
   resources :post_images, only: [:new, :index, :show, :create, :destroy]
   resources :users, only: [:show, :edit]
-  
-  devise_for :users
+
+
 
 
   get 'homes/about' => 'homes#about', as: 'about'
