96094bb0e56cbfcbfc78e5506ca7b11827045f84
AyakaTakahashi <132245813+suco31a42@users.noreply.github.com>
2023-05-10 05:45:02 +0000

    [finish]4-18

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,7 +1,8 @@
 class PostImage < ApplicationRecord
   has_one_attached :image
-
   belongs_to :user
+  has_many :post_comments, dependent: :destroy
+
 
   def get_image
     unless image.attached?
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  get 'homes/about' => 'homes#about', as: 'about'
 
-  resources :post_images, only: [:new, :index, :show, :create, :destroy]
+  resources :post_images, only: [:new, :index, :show, :create, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
+
   resources :users, only: [:show, :edit, :update]
 
 
-
-
-  get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
