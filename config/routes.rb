27839d1b4a4ed69c42b559a6592aff33bd8d5880
AyakaTakahashi <132245813+suco31a42@users.noreply.github.com>
2023-05-10 02:26:39 +0000

    [finish]4-16

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
 
   resources :post_images, only: [:new, :index, :show, :create, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 
 
 
